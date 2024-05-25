b491b0bc91286192b78cb17441bf207a1ed7400b
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2024-05-26 00:15:28 +0200

    final edits

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
 
-const count = 10;
+const count = 30;
 const apiKey = `7-3AwowCpgOQQ8sAtplH6diTsRxr-EjH-p5vldmJjWw`;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Check if all images were loaded
+
+function imageLoaded() {
+    imagesLoaded++;
+    console.log(imagesLoaded);
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
 
 // Helper function to set attributes on DOM elements
 
@@ -21,6 +34,8 @@ function setAttributes(element, attributes) {
 // Create elements for links & photos, add to DOM
 
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     // Run function for each object in photosArray
     photosArray.forEach((photo) => {
 
@@ -39,6 +54,10 @@ function displayPhotos() {
             title: photo.alt_description,
         });
 
+        // Event listener, check when each is finished loading
+
+        img.addEventListener('load', imageLoaded);
+
         // Put <img> inside <a>, then put both inside imageContainer element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -53,16 +72,16 @@ async function getPhotos() {
         photosArray = await response.json();
         displayPhotos();
     } catch (error) {
-        // Catch error here
+        console.error('Error fetching photos:', error);
     }
 }
 
 // Check to see if scrolling near bottom of the page, load more photos
 
 window.addEventListener('scroll', () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
         getPhotos();
-        console.log('load more');
     }
 });
 
